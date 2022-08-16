9acc445eda049a633818ea2b75b85ca39f39b036
takatoh <takatoh.m@gmail.com>
2022-08-17 08:20:15 +0900

    get_tweeted_photo/tweeted_photo_downloader(TweetedPhotoDownloader): _pick_out() and _convert() methods are new, and rewrite get_by_id() with using its.

--- get_tweeted_photo/tweeted_photo_downloader.py
+++ get_tweeted_photo/tweeted_photo_downloader.py
@@ -13,34 +13,10 @@ class TweetedPhotoDownloader():
     def get_by_id(self, tweet_id):
         self.result = None
         status = self._api.get_status(tweet_id)
-        if 'extended_entities' in status._json and 'media' in status._json['extended_entities']:
-            entities = status._json['extended_entities']
-        elif 'entities' in status._json and 'media' in status._json['entities']:
-            entities = status._json['entities']
-        else:
-            return self.result
-        self.result = []
-        tweet = {
-            'screen_name' : status.user.screen_name,
-            'created_at' : status.created_at,
-            'id' : status.id,
-            'photos' : []
-        }
-        for media in entities['media']:
-            if media['type'] == 'photo':
-                sizes = {}
-                for k, v in media['sizes'].items():
-                    sizes[k] = {
-                        'w' : v['w'],
-                        'h' : v['h'],
-                        'resize' : v['resize']
-                    }
-                tweet['photos'].append({
-                    'media_url' : media['media_url_https'],
-                    'expanded_url' : media['expanded_url'],
-                    'sizes' : sizes
-                })
-        self.result.append(tweet)
+        tweet = self._pick_out(status)
+        if tweet is None:
+            return None
+        self.result = [tweet]
         return self.result
 
     def download_all(self, dir):
@@ -53,3 +29,36 @@ class TweetedPhotoDownloader():
                 f.write(res.content)
             count += 1
         return count
+
+    def _pick_out(self, status):
+        if 'extended_entities' in status._json and 'media' in status._json['extended_entities']:
+            entities = status._json['extended_entities']
+        elif 'entities' in status._json and 'media' in status._json['entities']:
+            entities = status._json['entities']
+        else:
+            return None
+        tweet = {
+            'screen_name' : status.user.screen_name,
+            'created_at' : status.created_at,
+            'id' : status.id,
+            'photos' : self._convert(entities)
+        }
+        return tweet
+
+    def _convert(self, entities):
+        photos = []
+        for media in entities['media']:
+            if media['type'] == 'photo':
+                sizes = {}
+                for k, v in media['sizes'].items():
+                    sizes[k] = {
+                        'w' : v['w'],
+                        'h' : v['h'],
+                        'resize' : v['resize']
+                    }
+                photos.append({
+                    'media_url' : media['media_url_https'],
+                    'expanded_url' : media['expanded_url'],
+                    'sizes' : sizes
+                })
+        return photos
