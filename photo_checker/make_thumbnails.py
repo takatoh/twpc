db3c4cfb701958e4518588afa2cb3159c5aa2de8
takatoh <takatoh.m@gmail.com>
2022-08-17 20:49:05 +0900

    photo_checker/make_thumbnails.py: --verbose option.

--- photo_checker/make_thumbnails.py
+++ photo_checker/make_thumbnails.py
@@ -13,13 +13,20 @@ def main():
     thumbs_dir = src_dir / THUMBNAIL_DIR
     thumbs_dir.mkdir(exist_ok=True)
 
+    count = 0
     for src in src_dir.iterdir():
         if not (src.is_file() and src.suffix in ['.png', '.jpg', '.jpeg']):
             continue
         thumb = thumbs_dir / src.name
         if thumb.exists():
             continue
+        if args.verbose:
+            print(f'{src}')
+            print(f'  => {thumb}')
         make_thumbnail(src, thumb)
+        count += 1
+    if args.verbose:
+        print(f'\n{count} thumbnails are made')
 
 
 def parse_arguments():
@@ -29,6 +36,11 @@ def parse_arguments():
         action='store',
         metavar='DIR',
         help='original image'
+    )
+    parser.add_argument(
+        '-v', '--verbose',
+        action='store_true',
+        help='verbose mode'
     )
     args = parser.parse_args()
     return args
