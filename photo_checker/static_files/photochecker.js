adb3538425e523339f0b70b8455de196d9659e66
takatoh <takatoh.m@gmail.com>
2022-08-18 19:29:14 +0900

    photo_checker/static_files/photochecker.js: Define function: postPhoto.

--- photo_checker/static_files/photochecker.js
+++ photo_checker/static_files/photochecker.js
@@ -2,3 +2,9 @@ const deletePhoto = (event) => {
     const fileName = event.target.closest(".row").id;
     alert("Delete file: " + fileName + " ?");
 };
+
+
+const postPhoto = (event) => {
+    const fileName = event.target.closest(".row").id;
+    alert("Post file: " + fileName + " ?");
+};
