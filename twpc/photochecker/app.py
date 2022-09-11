b15316229d83f1ae3f3fd45221b6d7f8fa1e9beb
takatoh <takatoh.m@gmail.com>
2022-09-11 13:02:20 +0900

    twpc/photochecker/app.py: Use load_config() function from twpc module.

--- twpc/photochecker/app.py
+++ twpc/photochecker/app.py
@@ -3,13 +3,11 @@ import requests
 from pathlib import Path
 import os
 import json
-from . import CONFIG_FILE_NAME
+from twpc import load_config
 from .photo_info_handler import PhotoInfoHandler
 
 
-config_file = Path(os.environ.get('HOME')) / CONFIG_FILE_NAME
-with open(config_file, 'r') as f:
-    CONFIG = json.load(f)
+CONFIG = load_config()
 TEMPLATE_PATH.append(Path(__file__).parent / 'views')
 STATIC_FILE_DIR = str(Path(__file__).parent / 'static_files')
 
